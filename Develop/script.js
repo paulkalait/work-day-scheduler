2f0e0d3a9db0fc606a55f460c82a0767534fe29e
Your Name <kalaitzidispaul@gmail.com>
2022-02-18 17:53:19 -0500

    added if statements to ccompare current time with time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,26 @@
-var currentDay = document.querySelector('#currentDay')
+var timeToday = document.querySelector('#currentDay')
 var currentTime = moment();
 
 
 //insert current day "<p> element" with moment.js
-currentDay.textContent = currentTime.format("MMM DD, YYYY - hh:mm:ss a")
+timeToday.textContent = currentTime.format("MMM DD, YYYY - hh:mm:ss a")
+
+// creates a current time variable to use to compare with
+var time = moment().hour()
+
+//loop over each time block using each, using parseInt to make it into integer 
+$(".time-block").each(function(){
+    var timeBlock = parseInt($(this).attr("class").split("hour")[1])
+
+    //if statemnt to check and color code time that is passed due
+    if(timeBlock < time){
+        $(this).removeClass("present")
+        $(this).removeClass("future")
+        $(this).addClass("past")
+    }
+    //else if statement to check if it is present with the current time
+
+
+
+    //else to check if the block time still has time
+})
