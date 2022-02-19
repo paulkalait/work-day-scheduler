8d8680f1fe40bc453d668a4763410a34d68f22bd
Your Name <kalaitzidispaul@gmail.com>
2022-02-18 21:13:18 -0500

    adding if statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
 var timeToday = document.querySelector('#currentDay')
 var currentTime = moment();
+var nine = "9"
 
 
 //insert current day "<p> element" with moment.js
@@ -9,8 +10,8 @@ timeToday.textContent = currentTime.format("MMM DD, YYYY - hh:mm:ss a")
 var time = moment().hour()
 
 //loop over each time block using each, using parseInt to make it into integer 
-$(".time-block").each(function(){
-    var timeBlock = parseInt($(this).attr("class").split("hour")[1])
+$("#hour-").each(function(){
+    var timeBlock = parseInt($(this).attr("id"))
 
     //if statemnt to check and color code time that is passed due
     if(timeBlock < time){
@@ -21,6 +22,14 @@ $(".time-block").each(function(){
     //else if statement to check if it is present with the current time
 
 
-
+    console.log(timeBlock)
     //else to check if the block time still has time
 })
+
+// var nine = $("#hour-9")
+
+// var Convert = function(){
+//     moment(nine).set('hour', 9)
+//     console.log(nine)
+// }
+// Convert();
